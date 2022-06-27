1878390baf33f11578205fd570c6781d37fc31f0
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-27 17:42:18 +0530

    feat: added footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import styles from "../styles/Footer.module.css";
+import Image from "next/image"
 
-const Footer = () => {
+const  Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+     <div className={styles.item}>
+     <Image src="/img/bg.png" layout="fill" objectFit="cover"alt="" />
+     </div>
+     <div className={styles.item}>
+      <div className={styles.card}>
+      <h2 className={styles.motto}>Hurray! Bunny Pizzas.Well baked pizzas</h2>
+      </div>
+       <div className={styles.card}>
+        <h1 className={styles.title}>Find our restaurant</h1>
+        <p className={styles.text}>
+        Near Venkatesware Temple,
+        Asilmetta
+        <br/> Visakhapatnam
+        <br/> 012345678
+        </p>
+        <p className={styles.text}>
+        4th lane,
+        Beach Road
+        <br/> Visakhapatnam
+        <br/> 012345678
+        </p>
+        <p className={styles.text}>
+        4th lane,
+        Near RK Beach
+        <br/> Visakhapatnam
+        <br/> 012345678
+        </p>
+        <p className={styles.text}>
+        MVP Colony,
+        Near laddu gopal
+        <br/> Visakhapatnam
+        <br/> 012345678
+        </p>
+       </div>
+        <div className={styles.card}>
+         <h1 className={styles.title}>Restaurant timings</h1>
+          <p className={styles.text}>
+          Monday until Friday
+          <br/> 9:00AM-10:00PM
+          </p>
+          <p className={styles.text}>
+          Saturday and Sunday
+          <br/> 2:00PM-8:00PM
+          </p>
+        </div>
+     </div>
+    </div>
   )
 }
-
 export default Footer
