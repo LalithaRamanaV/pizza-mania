d3f48ce9b3c39fb80e8e54cc9f4d846baa9eadfa
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2023-02-11 18:25:55 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home({ pizzaList, admin }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
-      {<AddButton setClose={setClose} />}
+      {/* {<AddButton setClose={setClose} />} */}
       <PizzaList pizzaList={pizzaList} />
       {!close && <Add setClose={setClose} />}
     </div>
