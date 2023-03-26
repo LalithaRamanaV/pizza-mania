a073c26a4d31fec3e0a4f9d9a17a75eddcf7e761
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2023-03-27 03:40:09 +0530

    fix: navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -21,12 +21,12 @@ const Navbar = () => {
       <Link href="/" passHref>
         <li className={styles.listItem}>Home</li>
         </Link>
-         <li className={styles.listItem}>Products</li>
-         <li className={styles.listItem}>Offers</li>
+         {/* <li className={styles.listItem}>Products</li>
+         <li className={styles.listItem}>Offers</li> */}
         <Image src="/img/logo4.png" alt=""  width="160px" height="169px" />
-        <li className={styles.listItem}>Blog</li>
-        <li className={styles.listItem}>Contact</li>
-        <li className={styles.listItem}>Event</li>
+        <li className={styles.listItem}>Products</li>
+        {/* <li className={styles.listItem}>Contact</li>
+        <li className={styles.listItem}>Event</li> */}
       </ul>
      </div>
      <Link href="/cart" passHref>
